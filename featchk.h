c0cfcf0cbc88286396b5cde4b7b37f778a73f131
s_dogood17060117 <s_dogood17060117@yahoo.com>
2011-08-31 15:09:16 +0000

    Hercules SVN 7734 - Provide dummy SVS Clear Global Summary to prevent kernel panics in older linux kernels

--- featchk.h
+++ featchk.h
@@ -1,5 +1,9 @@
 /* FEATCHK.H    (c) Copyright Jan Jaeger, 2000-2011                  */
 /*              Feature definition consistency checks                */
+/*                                                                   */
+/*   Released under "The Q Public License Version 1"                 */
+/*   (http://www.hercules-390.org/herclic.html) as modifications to  */
+/*   Hercules.                                                       */
 
 // $Id: featchk.h 7734 2011-08-31 12:38:58Z jj $
 
