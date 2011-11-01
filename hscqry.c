2131807e2ba9a1773fc8b02bd7e916e7c15e725c
s_dogood17060117 <s_dogood17060117@yahoo.com>
2011-11-01 00:50:27 +0000

    New templates provided for VM style commands modules

---
+++ hscqry.c
@@ -0,0 +1,73 @@
+/* HSCQRY.C     (c) Copyright Silence Dogood, 2011                   */
+/*              Execute Hercules System Commands - QUERY             */
+/*                                                                   */
+/*   Released under "The Q Public License Version 1"                 */
+/*   (http://www.hercules-390.org/herclic.html) as modifications to  */
+/*   Hercules.                                                       */
+
+// $Id$
+
+/*-------------------------------------------------------------------*/
+/* This module implements the various Hercules System Console        */
+/* QUERY commands.                                                   */
+/*-------------------------------------------------------------------*/
+
+/*
+   Standard conventions are:
+
+     argc             contains the number of elements in argv[]
+     argv[0]          contains the actual command
+     argv[1..argc-1]  contains the optional arguments
+     cmdline          contains the original command line
+
+     returncode:
+
+     0 = Success
+
+     < 0 Error: Command not executed
+
+     > 1 Failure:  one or more functions could not complete
+
+   int test_cmd(int argc, char *argv[], char *cmdline)
+   {
+
+   .
+   .
+   .
+   return rc
+
+   }
+
+*/
+
+#include "hstdinc.h"
+
+#define _HSCQRY_C_
+#define _HENGINE_DLL_
+
+#include "hercules.h"
+#include "devtype.h"
+#include "opcode.h"
+#include "history.h"
+#include "httpmisc.h"
+
+#if defined(OPTION_FISHIO)
+#include "w32chan.h"
+#endif /* defined(OPTION_FISHIO) */
+
+#include "tapedev.h"
+#include "dasdtab.h"
+#include "ctcadpt.h"
+
+// (forward references, etc)
+
+#define MAX_DEVLIST_DEVICES  1024
+
+#if defined(FEATURE_ECPSVM)
+extern void ecpsvm_command( int argc, char **argv );
+#endif
+int HercCmdLine ( char *cmdline );
+int exec_cmd(int argc, char *argv[],char *cmdline);
+
+/* HSCQRY.C End-of-text */
+
