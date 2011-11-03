db467e755d0709c149de5df8328d197bb11c7563
s_dogood17060117@yahoo.com <s_dogood17060117@yahoo.com@da7438f3-5229-ee6a-cecb-62d722bd0f8f>
2011-11-03 17:18:03 +0000

    New templates provided for VM style commands modules - CONT

--- trunk/cmdtabvq.h
+++ trunk/cmdtabvq.h
@@ -32,12 +32,40 @@
   #define _FW_REF_ABR(_s,_a,_f,_t,_d,_l)  static int (_f)( CMDFUNC_ARGS_PROTO );
 #endif
 
-CMDABBR( "help",   4,              help_cmd,              SYSCMD,             help_cmd_desc,         help_cmd_help     )
+#if 0               // Comments from cmdinc.h
+/*-------------------------------------------------------------------*/
+/* Command types                                                     */
+/*-------------------------------------------------------------------*/
+#define DISABLED       0x00             /* Disabled command          */
+#define SYSOPER        0x01             /* System Operator           */
+#define SYSMAINT       0x02             /* System Maintainer         */
+#define SYSPROG        0x04             /* Systems Programmer        */
+#define SYSNONE        0x08             /* Valid with no group       */
+#define SYSCONFIG      0x10             /* System Configuration      */
+#define SYSDEVEL       0x20             /* System Developer          */
+#define SYSDEBUG       0x40             /* Enable Debugging          */
+#define SYSNDIAG8      0x80             /* Invalid for DIAG008       */
+#define SYSALL         0x7F             /* Enable for any lvls       */
+
+/* Combinations as a one-word mnemonic */
+
+#define SYSCMD              (SYSOPER | SYSMAINT | SYSPROG | SYSCONFIG | SYSDEVEL | SYSDEBUG)
+#define SYSCMDNDIAG8        (SYSCMD    | SYSNDIAG8)
+#define SYSCFGNDIAG8        (SYSCONFIG | SYSNDIAG8)
+#define SYSALLNDIAG8        (SYSALL    | SYSNDIAG8)
+#define SYSCMDNOPER         (SYSCMD       - SYSOPER)
+#define SYSCMDNOPERNDIAG8   (SYSCMDNDIAG8 - SYSOPER)
+#define SYSCMDNOPERNPROG    (SYSCMDNOPER  - SYSPROG)
+#define SYSPROGDEVEL        (SYSPROG | SYSDEVEL)
+#endif
+
+// Keep these sorted ...
 CMDABBR( "cpuid",  5,              cpuid_cmd,             SYSCMD,             cpuid_cmd_desc,        cpuid_cmd_help     )
+CMDABBR( "help",   4,              help_cmd,              SYSCMD,             help_cmd_desc,         help_cmd_help     )
+#if defined( OPTION_CONFIG_SYMBOLS )
+CMDABBR( "pfkeys",  2,             pfkeys_cmd,            SYSCMD,             pfkeys_cmd_desc,       NULL                )
+#endif
 CMDABBR( "pid",    3,              pid_cmd,               SYSCMD,             pid_cmd_desc,          NULL                )
 CMDABBR( "ports",  2,              ports_cmd,             SYSCMD,             ports_cmd_desc,        NULL                )
 CMDABBR( "processor",4,            proc_cmd,              SYSCMD,             proc_cmd_desc,         NULL                )
 CMDABBR( "storage", 3,             stor_cmd,              SYSCMD,             stor_cmd_desc,         NULL                )
-#if defined( OPTION_CONFIG_SYMBOLS )
-CMDABBR( "pfkeys",  2,             pfkeys_cmd,            SYSCMD,             pfkeys_cmd_desc,       NULL                )
-#endif
--- trunk/hscset.c
+++ trunk/hscset.c
@@ -59,12 +59,72 @@
 #include "dasdtab.h"
 #include "ctcadpt.h"
 
-int set_vm_cmd(int argc, char *argv[],char *cmdline)
+int set_vm_cmd(int argi, char *argx[],char *cmdline)
 {
+    char           *argn[MAX_ARGS+1];   // Our copy of pointers
+    char          **argv = argn;        // Our copy of the ptr to pointers
+    int             argc;               // Our copy
+    CMDTAB*         pCmdTab;
+    size_t          cmdlen, matchlen;
+    int             rc;                 // Default to invalid command
+
     UNREFERENCED(argc);
     UNREFERENCED(argv);
     UNREFERENCED(cmdline);
+
+    VERIFY( argi <= MAX_ARGS );                     // This should always be true
+    argc = ( argi <= MAX_ARGS ? argi : MAX_ARGS );
+    for( rc = 0; rc < argc; rc++ )
+        argn[rc] = argx[rc];
+
+    argc--;         // Decrement Count to skip past "QUERY"
+    argv++;         // Move pointer forward past "QUERY"
+    
+    rc = HERRINVCMD;
+    
+    for (pCmdTab = cmdtab; pCmdTab->statement; pCmdTab++)
+    {
+        if (1
+            && pCmdTab->function
+            && (pCmdTab->type & sysblk.sysgroup)
+            /* Commands issues through DIAG8 must NOT be part of the SYSNDIAG8 group */
+            && (!(sysblk.diag8cmd & DIAG8CMD_RUNNING) || !(pCmdTab->type & SYSNDIAG8))
+        )
+        {
+            cmdlen = pCmdTab->mincmdlen ? pCmdTab->mincmdlen : strlen( pCmdTab->statement );
+            matchlen = MAX( strlen(argv[0]), cmdlen );
+
+            if (!strncasecmp( argv[0], pCmdTab->statement, matchlen ))
+            {
+                char cmd[256]; /* (copy of command name) */
+
+                /* Make full-length copy of the true command's name */
+                strlcpy( cmd, pCmdTab->statement, sizeof(cmd) );
+                argv[0] = cmd; /* (since theirs may be abbreviated) */
+
+                 /* Process the Hercules command */
+                rc = pCmdTab->function( CMDFUNC_ARGS );
+                break;
+            }
+            /* end strncasecmp( table entry match ) */
+        }
+        /* end if ( valid table entry ) */
+    }
+    /* end for ( search table ) */
     return 0;
 }
+
+/*-------------------------------------------------------------------*/
+/* help command                                                      */
+/*-------------------------------------------------------------------*/
+static int help_cmd(int argc, char *argv[], char *cmdline)
+{
+    UNREFERENCED(cmdline);
+    UNREFERENCED(argv);
+    UNREFERENCED(argc);
+    return 0;
+}
+
 /* HSCSET.C End-of-text */
+#undef _HSCSET_C_
 
